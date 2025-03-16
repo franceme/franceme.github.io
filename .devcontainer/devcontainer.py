a063e82e60967982e8f12abeee2f4f28a3b36fac
franceme <franceme@users.noreply.github.com>
2025-03-16 23:52:55 +0000

    deploy: ff3d321ed07296dfbedfba379275c909e147b52e

--- .devcontainer/devcontainer.py
+++ .devcontainer/devcontainer.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
-import os,sys
+import os
+import sys
+import pip
 run = lambda x:os.system(x)
 
-for x in ['Gruntfuggly.todo-tree', 'ms-python.python', 'actboy168.tasks', "MS-SarifVSCode.sarif-viewer",'dchanco.vsc-invoke', 'donjayamanne.githistory', 'alefragnani.Bookmarks', 'littlefoxteam.vscode-python-test-adapter', 'njpwerner.autodocstring', 'sourcery.sourcery', 'GitHub.copilot', 'hbenl.vscode-test-explorer','ritwickdey.LiveServer']:
+for x in [
+	'Gruntfuggly.todo-tree',
+	'ms-python.python',
+	'actboy168.tasks',
+	'MS-SarifVSCode.sarif-viewer',
+	'dchanco.vsc-invoke',
+	'donjayamanne.githistory',
+	'alefragnani.Bookmarks',
+	'littlefoxteam.vscode-python-test-adapter',
+	'njpwerner.autodocstring',
+	'sourcery.sourcery',
+	'GitHub.copilot',
+	'hbenl.vscode-test-explorer',
+	'ritwickdey.LiveServer'
+]:
 	run(f"code-server --install-extension {x}")
+
+requirements = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", "", "requirements.txt")
+if os.path.exists(requirements):
+	pip.main(f"install -r {requirements}".split())
